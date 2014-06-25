63e7c09756f3df196a3e998b77e58df955dd1e08
Tamer Qtaish <TamerQtaish@gmail.com>
2014-06-25 16:12:05 +0000

    new changes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,157 +4,108 @@ class UserController extends BaseController {
 
 	public function getRegister()
 	{
-		try {
-			$input = Input::old();
+		$input = Input::old();
 
-			return View::make('index', array(
-						'title' => Lang::get('user.register.title')
-						))
-				->nest('viewBody', 'user.register', array(
-							'input' => $input
-							));
-
-		} catch (Exception $e) {
-			// Return to form
-			return Redirect::to('/')
-				->withInput()
-				->with('notification',
-						array('type' => 'error','message' => $e->getMessage()
-						     ));
-		}
+		return View::make('index', array(
+					'title' => Lang::get('user.register.title')
+					))
+			->nest('viewBody', 'user.register', array(
+						'input' => $input
+						));
 	}
 
 	public function PostRegister()
 	{
-		try {
-			$input = Input::all();
+		$input = Input::all();
 
-			$rules = array('first_name' => 'required',
-					'last_name' => 'required',
-					'accept_t_and_c' => 'required',
-					'password' => 'required|min:8|confirmed',
-					'email' => 'required|email'
-				      );
+		$rules = array('first_name' => 'required',
+				'last_name' => 'required',
+				'accept_t_and_c' => 'required',
+				'password' => 'required|min:8|confirmed',
+				'email' => 'required|email'
+			      );
 
-			$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make(Input::all(), $rules);
 
-			if ($validator->fails()) {
-				return Redirect::to('user/register')->withInput()
-					->with('notification', array(
-								'type' => 'error',
-								'message' => $validator->errors()
-								));
-			}
-
-			if(User::checkEmailExists($input['email'])) {
-				return Redirect::to('user/register')->withInput()
-					->with('notification', array(
-								'type' => 'error',
-								'message' => Lang::get('user.register.errors.email_used')
-								));                            
-			}
-
-			$user = new User;
-			$user->email = $input['email'];
-			$user->password = Hash::make($input['password']);
-			$user->first_name = $input['first_name'];
-			$user->last_name = $input['last_name'];
-			$user->phone = $input['phone'];
-			$user->t_and_c = (isset($data['accept_t_and_c'])) ? 1 : 0;
-			$user->signup_ip = Request::getClientIp();
-
-			$user->save();
-
-			return View::make('index', array(
-						'title' => Lang::get('user.register_success.title')
-						))
-				->nest('viewBody', 'user.register_success', array());
-
-		} catch (Exception $e) {
-			return Redirect::to('/')
-				->withInput()
-				->with('notification',
-						array('type' => 'error','message' => $e->getMessage()
-						     ));
+		if ($validator->fails()) {
+			return Redirect::to('user/register')->withInput()
+				->with('notification', array(
+							'type' => 'error',
+							'message' => $validator->errors()
+							));
 		}
+
+		if(User::checkEmailExists($input['email'])) {
+			return Redirect::to('user/register')->withInput()
+				->with('notification', array(
+							'type' => 'error',
+							'message' => Lang::get('user.register.errors.email_used')
+							));                            
+		}
+
+		$user = new User;
+		$user->email = $input['email'];
+		$user->password = Hash::make($input['password']);
+		$user->first_name = $input['first_name'];
+		$user->last_name = $input['last_name'];
+		$user->phone = $input['phone'];
+		$user->t_and_c = (isset($data['accept_t_and_c'])) ? 1 : 0;
+		$user->signup_ip = Request::getClientIp();
+
+		$user->save();
+
+		return View::make('index', array(
+					'title' => Lang::get('user.register_success.title')
+					))
+			->nest('viewBody', 'user.register_success', array());
 	}
 
 	public function getLogin()
 	{
-		try {
-			$input = Input::old();
+		$input = Input::old();
 
-			return View::make('index', array(
-						'title' => Lang::get('user.login.title')
-						))
-				->nest('viewBody', 'user.login', array(
-							'input' => $input
-							));
-
-		} catch (Exception $e) {
-			// Return to form
-			return Redirect::to('/')
-				->withInput()
-				->with('notification',
-						array('type' => 'error','message' => $e->getMessage()
-						     ));
-		}
+		return View::make('index', array(
+					'title' => Lang::get('user.login.title')
+					))
+			->nest('viewBody', 'user.login', array(
+						'input' => $input
+						));
 	}
 
 	public function postLogin()
 	{
-		try {
-			$input = Input::all();
+		$input = Input::all();
 
-			$rules = array('password' => 'required|min:8',
-					'email' => 'required|email'
-				      );
+		$rules = array('password' => 'required|min:8',
+				'email' => 'required|email'
+			      );
 
-			$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make(Input::all(), $rules);
 
-			if ($validator->fails()) {
-				return Redirect::to('user/login')->withInput()
-					->with('notification', array(
-								'type' => 'error',
-								'message' => $validator->errors()
-								));
-			}
-
-			if (Auth::attempt(array('email' => $input['email'], 'password' => $input['password']), (isset($input['remember_me']) ? 1 : 0))) {
-				$user = Auth::user();
-				$user->last_on = time();
-				$user->save();
-
-				return Redirect::to('/');
-			} else {
-				return Redirect::to('/user/login')->with('notification', array(
-							'type' => 'error',
-							'message' => Lang::get('user.login.error')
-							));
-			}
-		} catch (Exception $e) {
-			// Return to form
-			return Redirect::to('/')
-				->withInput()
-				->with('notification',
-						array('type' => 'error','message' => $e->getMessage()
-						     ));
-		}
-	}
-        
-        public function logout() {
-		try {
-			Auth::logout();
-
-			return Redirect::to('/');
-		} catch (Exception $e) {
-			// Return to edit
-			return Redirect::to('/')
-				->withInput()
+		if ($validator->fails()) {
+			return Redirect::to('user/login')->withInput()
 				->with('notification', array(
 							'type' => 'error',
-							'message' => $e->getMessage()
+							'message' => $validator->errors()
 							));
-		}            
-        }
+		}
+
+		if (Auth::attempt(array('email' => $input['email'], 'password' => $input['password']), (isset($input['remember_me']) ? 1 : 0))) {
+			$user = Auth::user();
+			$user->last_on = time();
+			$user->save();
+
+			return Redirect::to('/');
+		} else {
+			return Redirect::to('/user/login')->with('notification', array(
+						'type' => 'error',
+						'message' => Lang::get('user.login.error')
+						));
+		}
+	}
+
+	public function logout() {
+		Auth::logout();
+		return Redirect::to('/');
+	}
 }
